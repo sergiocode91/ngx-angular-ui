e2ba13938659eca144606bac21c9d5631127d670
sergiodev91 <sergiodev91@gmail.com>
2024-06-08 10:48:16 -0400

    ✨ [ADD] New components buttons

--- projects/documentation/src/app/docs/button/button.component.ts
+++ projects/documentation/src/app/docs/button/button.component.ts
@@ -71,6 +71,100 @@ export class ButtonComponent {
   })
   `;
 
+  public code2 = `
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlButtonDirective],
+    template: \`<button uiButton variant="secondary">Button</button>\`
+  })
+  `;
+
+  public code3 = `
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlButtonDirective],
+    template: \`<button uiButton variant="outline">Outline</button>\`
+  })
+  `;
+
+  public code4 = `
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlButtonDirective],
+    template: \`<button uiButton variant="destructive">Destructive</button>\`
+  })
+  `;
+
+  public code5 = `
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlButtonDirective],
+    template: \`<button uiButton variant="ghost">Get started</button>\`
+  })
+  `;
+
+  public code6 = `
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlButtonDirective],
+    template: \`<button uiButton variant="link">Don’t have an account?</button>\`
+  })
+  `;
+
+  public code7 = `
+  import { LucideAngularModule } from 'lucide-angular';
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [LucideAngularModule, HtmlButtonDirective],
+    template: \`
+    <button uiButton variant="outline" size="icon">
+      <lucide-icon name="Moon" [size]="18"></lucide-icon>
+    </button>\`
+  })
+  `;
+
+  public code8 = `
+  import { LucideAngularModule } from 'lucide-angular';
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [LucideAngularModule, HtmlButtonDirective],
+    template: \`
+    <button uiButton variant="primary">
+      <lucide-icon name="Instagram" [size]="18" class="inline-flex mr-1"></lucide-icon>
+      Sign in with Instagram
+    </button>\`
+  })
+  `;
+
+  public code9 = `
+  import { LucideAngularModule } from 'lucide-angular';
+  import { HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [LucideAngularModule, HtmlButtonDirective],
+    template: \`
+    <button uiButton variant="destructive" disabled="true">
+      <lucide-icon name="Trash" [size]="18" class="inline-flex mr-1"></lucide-icon>
+      Delete account
+    </button>\`
+  })
+  `;
+
   constructor() {
     this.initializeTabs(9);
   }
