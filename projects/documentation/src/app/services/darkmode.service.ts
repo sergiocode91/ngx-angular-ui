7e70d2938478e97d617d8da558de78c461ba60f9
sergiodev91 <sergiodev91@gmail.com>
2024-06-09 10:21:29 -0400

    🔧 [FIX] Fix class en html darkmode

--- projects/documentation/src/app/services/darkmode.service.ts
+++ projects/documentation/src/app/services/darkmode.service.ts
@@ -1,21 +1,38 @@
-import { Injectable, effect, signal } from '@angular/core';
+import { Injectable, effect, signal, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkmodeService {
-
   public darkModeSignal = signal<string>(
     window.localStorage.getItem('darkMode') || 'dark'
   );
+
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    
+    this.updateHtmlClass(this.darkModeSignal());
+
+    effect(() => {
+      const currentMode = this.darkModeSignal();
+      window.localStorage.setItem('darkMode', currentMode);
+      this.updateHtmlClass(currentMode);
+    });
+  }
 
   public toggleDarkmode() {
     this.darkModeSignal.update((value) => value === 'light' ? 'dark' : 'light');
   }
 
-  constructor() {
-    effect(() => {
-      window.localStorage.setItem('darkMode', this.darkModeSignal());
-    });
+  private updateHtmlClass(mode: string) {
+    const htmlElement = document.documentElement;
+    if (mode === 'dark') {
+      this.renderer.addClass(htmlElement, 'dark');
+    } else {
+      this.renderer.removeClass(htmlElement, 'dark');
+    }
   }
 }
+
