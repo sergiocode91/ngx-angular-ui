8ea2ea024701c49dbdc7d8fd179e5147538ebac4
sergiodev91 <sergiodev91@gmail.com>
2024-06-09 19:56:57 -0400

    🔧 [FIX] Order classes

--- projects/ngx-angular-ui/src/lib/input/input.directive.ts
+++ projects/ngx-angular-ui/src/lib/input/input.directive.ts
@@ -25,7 +25,8 @@ export class HtmlInputDirective implements OnInit, OnChanges {
   }
 
   private applyClasses() {
-    const baseClasses = 'w-full h-9 flex font-normal text-sm placeholder:text-zinc-400 px-3 py-1 shadow-sm rounded-md bg-transparent border border-input focus:outline-2 focus:outline-neutral-500 disabled:cursor-not-allowed disabled:opacity-50';
+    const baseClasses =
+      'w-full h-9 flex font-normal text-sm placeholder:text-zinc-500 px-3 py-1 shadow-sm rounded-md bg-transparent border border-input focus:outline-2 focus:outline-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-white dark:border-zinc-800 dark:focus:outline-zinc-800';
     const classes = `${baseClasses} ${this.class}`;
 
     this.renderer.setAttribute(this.el.nativeElement, 'class', classes);
