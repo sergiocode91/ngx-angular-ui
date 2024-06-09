12020066a27e4a6f6e17c59af25d0507915291bb
sergiodev91 <sergiodev91@gmail.com>
2024-06-09 19:31:45 -0400

    ✨ [ADD] New component input

--- projects/documentation/src/app/docs/input/input.component.ts
+++ projects/documentation/src/app/docs/input/input.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
-import { HeadingComponent } from '@components/index';
+import {
+  HeadingComponent,
+  HighlightCodeComponent,
+  TabsComponent,
+} from '@components/index';
+import { HtmlInputDirective } from '../../../../../ngx-angular-ui/src/lib/input/input.directive';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [HeadingComponent],
+  imports: [
+    HeadingComponent,
+    HtmlInputDirective,
+    HighlightCodeComponent,
+    TabsComponent,
+  ],
   templateUrl: './input.component.html',
-  styles: ``
+  styles: ``,
 })
 export class InputComponent {
+  public activeTabs: { [key: string]: string } = {};
 
+  public codeUseTs = `
+  import { HtmlInputDirective } from 'ngx-angular-ui';
+
+  @Component({
+    standalone: true,
+    imports: [HtmlInputDirective],
+  })
+  `;
+
+  public codeUseHtml = `
+  <input uiInput type="email" placeholder="Email">
+  `;
+
+  public code1 = `
+  import { HtmlInputDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlInputDirective],
+    template: \`
+      <input uiInput type="email" placeholder="Enter your email">
+    \`
+  })
+  `;
+
+  constructor() {
+    this.initializeTabs(4);
+  }
+
+  initializeTabs(numberOfTabs: number) {
+    for (let i = 1; i <= numberOfTabs; i++) {
+      this.activeTabs[`tab${i}`] = 'Preview';
+    }
+  }
+
+  changeTab(tabId: string, newActiveTab: string) {
+    this.activeTabs[tabId] = newActiveTab;
+  }
 }
---
+++ projects/ngx-angular-ui/src/lib/input/input.directive.ts
@@ -0,0 +1,33 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[uiInput]',
+  standalone: true,
+})
+export class HtmlInputDirective implements OnInit, OnChanges {
+  @Input() class: string = '';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.applyClasses();
+  }
+
+  ngOnChanges() {
+    this.applyClasses();
+  }
+
+  private applyClasses() {
+    const baseClasses = 'w-full h-9 flex font-normal text-sm placeholder:text-zinc-400 px-3 py-1 shadow-sm rounded-md bg-transparent border border-input focus:outline-2 focus:outline-neutral-500 disabled:cursor-not-allowed disabled:opacity-50';
+    const classes = `${baseClasses} ${this.class}`;
+
+    this.renderer.setAttribute(this.el.nativeElement, 'class', classes);
+  }
+}
