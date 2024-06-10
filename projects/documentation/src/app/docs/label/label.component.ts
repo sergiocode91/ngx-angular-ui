11f52a012b5abb6e2d506f2c5118d6df80e66a4d
sergiodev91 <sergiodev91@gmail.com>
2024-06-10 00:06:07 -0400

    ✨ [ADD] Component label

--- projects/documentation/src/app/docs/label/label.component.ts
+++ projects/documentation/src/app/docs/label/label.component.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
-import { HeadingComponent } from '@components/index';
+import {
+  HeadingComponent,
+  HighlightCodeComponent,
+  TabsComponent,
+} from '@components/index';
+import { HtmlLabelDirective } from '../../../../../ngx-angular-ui/src/lib/label/label.directive';
+import { HtmlInputDirective } from '../../../../../ngx-angular-ui/src/lib/input/input.directive';
 
 @Component({
   selector: 'app-label',
   standalone: true,
-  imports: [HeadingComponent],
+  imports: [
+    HeadingComponent,
+    HighlightCodeComponent,
+    HtmlLabelDirective,
+    HtmlInputDirective,
+    TabsComponent,
+  ],
   templateUrl: './label.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LabelComponent {
+  public activeTabs: { [key: string]: string } = {};
 
+  public codeUseTs = `
+  import { HtmlLabelDirective } from 'ngx-angular-ui';
+
+  @Component({
+    standalone: true,
+    imports: [HtmlLabelDirective],
+  })
+  `;
+
+  public codeUseHtml = `
+  <label uiLabel>Username</label>
+  `;
+
+  public code = `
+  import { HtmlLabelDirective, HtmlInputDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlLabelDirective, HtmlInputDirective],
+    template: \`
+      <div class="flex flex-col space-y-2">
+        <label uiLabel for="Username">Username</label>
+        <input uiInput type="text" id="Username" placeholder="Write your username">
+      </div>
+    \`
+  })
+  `;
+
+  constructor() {
+    this.initializeTabs(4);
+  }
+
+  initializeTabs(numberOfTabs: number) {
+    for (let i = 1; i <= numberOfTabs; i++) {
+      this.activeTabs[`tab${i}`] = 'Preview';
+    }
+  }
+
+  changeTab(tabId: string, newActiveTab: string) {
+    this.activeTabs[tabId] = newActiveTab;
+  }
 }
