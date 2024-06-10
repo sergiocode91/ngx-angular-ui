708bc1f61c30cb3a2b3daf52278e7d7677aec6ed
sergiodev91 <sergiodev91@gmail.com>
2024-06-10 14:18:25 -0400

    ✨ [ADD] Components textarea variants

--- projects/documentation/src/app/docs/textarea/textarea.component.ts
+++ projects/documentation/src/app/docs/textarea/textarea.component.ts
@@ -1,13 +1,104 @@
 import { Component } from '@angular/core';
-import { HeadingComponent } from '@components/index';
+import { HeadingComponent, HighlightCodeComponent, TabsComponent } from '@components/index';
+import { HtmlTextareaDirective } from '../../../../../ngx-angular-ui/src/lib/textarea/textarea.directive';
+import { HtmlLabelDirective } from '../../../../../ngx-angular-ui/src/lib/label/label.directive';
+import { HtmlButtonDirective } from '../../../../../ngx-angular-ui/src/public-api';
 
 @Component({
   selector: 'app-textarea',
   standalone: true,
-  imports: [HeadingComponent],
+  imports: [
+    HeadingComponent,
+    HighlightCodeComponent,
+    HtmlTextareaDirective,
+    HtmlLabelDirective,
+    HtmlButtonDirective,
+    TabsComponent,
+  ],
   templateUrl: './textarea.component.html',
   styles: ``
 })
 export class TextareaComponent {
+  public activeTabs: { [key: string]: string } = {};
 
+  public codeUseTs = `
+  import { HtmlTextareaDirective } from 'ngx-angular-ui';
+
+  @Component({
+    standalone: true,
+    imports: [HtmlTextareaDirective],
+  })
+  `;
+
+  public codeUseHtml = `
+  <textarea uiTextarea placeholder="Type your message here."></textarea>
+  `;
+
+  public code1 = `
+  import { HtmlTextareaDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlTextareaDirective],
+    template: \`
+      <textarea uiTextarea placeholder="Type your message here."></textarea>
+    \`
+  })
+  `;
+
+  public code2 = `
+  import { HtmlTextareaDirective, HtmlLabelDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlTextareaDirective, HtmlLabelDirective],
+    template: \`
+      <div class="flex flex-col space-y-2">
+        <label uiLabel for="email">Message</label>
+        <textarea uiTextarea placeholder="Type your message here."></textarea>
+      </div>
+    \`
+  })
+  `;
+
+  public code3 = `
+  import { HtmlInputDirective, HtmlButtonDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlInputDirective, HtmlButtonDirective],
+    template: \`
+      <div class="flex flex-col items-end space-y-4">
+        <textarea uiTextarea placeholder="Type your message here."></textarea>
+        <button uiButton>Send</button>
+      </div>
+    \`
+  })
+  `;
+
+  public code4 = `
+  import { HtmlTextareaDirective } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [HtmlTextareaDirective],
+    template: \`
+      <textarea uiTextarea placeholder="Type your message here." disabled></textarea>
+    \`
+  })
+  `;
+
+  constructor() {
+    this.initializeTabs(4);
+  }
+
+  initializeTabs(numberOfTabs: number) {
+    for (let i = 1; i <= numberOfTabs; i++) {
+      this.activeTabs[`tab${i}`] = 'Preview';
+    }
+  }
+
+  changeTab(tabId: string, newActiveTab: string) {
+    this.activeTabs[tabId] = newActiveTab;
+  }
 }
---
+++ projects/ngx-angular-ui/src/lib/textarea/textarea.directive.ts
@@ -0,0 +1,34 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[uiTextarea]',
+  standalone: true,
+})
+export class HtmlTextareaDirective implements OnInit, OnChanges {
+  @Input() class: string = '';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.applyClasses();
+  }
+
+  ngOnChanges() {
+    this.applyClasses();
+  }
+
+  private applyClasses() {
+    const baseClasses =
+      'w-full min-h-16 flex bg-transparent border border-input rounded-md px-3 py-2 font-normal text-sm placeholder:text-zinc-400 shadow-sm focus:outline-2 focus:outline-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:text-white dark:placeholder:text-white/80 dark:border-zinc-800 dark:focus:outline-zinc-800';
+    const classes = `${baseClasses} ${this.class}`;
+
+    this.renderer.setAttribute(this.el.nativeElement, 'class', classes);
+  }
+}
