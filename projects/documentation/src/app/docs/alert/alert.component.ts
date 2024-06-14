9d90047a589e650c8bbd8f07c8901436c20f3c31
sergiodev91 <sergiodev91@gmail.com>
2024-06-14 19:00:19 -0400

    🔧 [UPDATE] Change color borders

--- projects/documentation/src/app/docs/alert/alert.component.ts
+++ projects/documentation/src/app/docs/alert/alert.component.ts
@@ -88,7 +88,7 @@ export class AlertComponent {
     template: \`
       <div uiAlert variant="primary">
         <div uiAlertTitle>Hello there!</div>
-        <div uiAlertDescription>Regular updates keep your app running smoothly.</div>
+        <div uiAlertDescription>Keep your app updated for best performance.</div>
       </div>
     \`
   })
@@ -109,22 +109,50 @@ export class AlertComponent {
       HtmlAlertDescriptionDirective
     ],
     template: \`
-    <div uiAlert variant="success">
-      <div uiAlertTitle>Success!</div>
-      <div uiAlertDescription>Your changes have been saved successfully.</div>
-    </div>
-    <div uiAlert variant="destructive">
-      <div uiAlertTitle>Error!</div>
-      <div uiAlertDescription>Unexpected error. Please try again.</div>
-    </div>
-    <div uiAlert variant="warning">
-      <div uiAlertTitle>Warning!</div>
-      <div uiAlertDescription>Unsaved changes detected. Please save your work.</div>
-    </div>
-    <div uiAlert variant="info">
-      <div uiAlertTitle>Info!</div>
-      <div uiAlertDescription>Save your work regularly to avoid data loss.</div>
-    </div>
+      <div uiAlert variant="primary" showIcon>
+        <lucide-icon name="Megaphone" [size]="18"></lucide-icon>
+        <div uiAlertTitle>Hello there!</div>
+        <div uiAlertDescription>Regular updates keep your app running smoothly.</div>
+      </div>
+    \`
+  })
+  `;
+
+  public code3 = `
+  import { 
+    HtmlAlertDirective, 
+    HtmlAlertTitleDirective,
+    HtmlAlertDescriptionDirective
+  } from 'ngx-angular-ui';
+  
+  @Component({
+    standalone: true,
+    imports: [
+      HtmlAlertDirective, 
+      HtmlAlertTitleDirective, 
+      HtmlAlertDescriptionDirective
+    ],
+    template: \`
+      <div uiAlert variant="success" showIcon>
+        <lucide-icon name="CircleCheck" [size]="18"></lucide-icon>
+        <div uiAlertTitle>Success!</div>
+        <div uiAlertDescription>Your changes have been saved successfully.</div>
+      </div>
+      <div uiAlert variant="destructive" showIcon>
+        <lucide-icon name="Ban" [size]="18"></lucide-icon>
+        <div uiAlertTitle>Error!</div>
+        <div uiAlertDescription>Unexpected error. Please try again.</div>
+      </div>
+      <div uiAlert variant="warning" showIcon>
+        <lucide-icon name="TriangleAlert" [size]="18"></lucide-icon>
+        <div uiAlertTitle>Warning!</div>
+        <div uiAlertDescription>Unsaved changes detected. Please save your work.</div>
+      </div>
+      <div uiAlert variant="info" showIcon>
+        <lucide-icon name="Info" [size]="18"></lucide-icon>
+        <div uiAlertTitle>Info!</div>
+        <div uiAlertDescription>Save your work regularly to avoid data loss.</div>
+      </div>
     \`
   })
   `;
