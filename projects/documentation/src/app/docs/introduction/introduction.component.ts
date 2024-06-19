2257eac66add555cb3053492bdaa47872bb3a25a
sergiodev91 <sergiodev91@gmail.com>
2024-06-19 17:53:09 -0400

    🔧 [FIX] Button styles

--- projects/documentation/src/app/docs/introduction/introduction.component.ts
+++ projects/documentation/src/app/docs/introduction/introduction.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeadingComponent } from '@components/index';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [HeadingComponent],
+  imports: [HeadingComponent, LucideAngularModule, CommonModule],
   templateUrl: './introduction.component.html',
   styles: ``
 })
 export class IntroductionComponent {
+  public activeIndex: number = 0;
+  public colors: string[] = ['#52525b', '#3b82f6', '#22c55e', '#6d28d9', '#ea580c', '#dc2626'];
 
+  setActive(index: number) {
+    this.activeIndex = index;
+  }
 }
