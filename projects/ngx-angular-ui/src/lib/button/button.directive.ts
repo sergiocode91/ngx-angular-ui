a7336178175e45222bbbef3cfae7d86ab2f4fd47
sergiodev91 <sergiodev91@gmail.com>
2024-06-30 08:55:05 -0400

    🔧 [update] Change variant

--- projects/ngx-angular-ui/src/lib/button/button.directive.ts
+++ projects/ngx-angular-ui/src/lib/button/button.directive.ts
@@ -12,15 +12,13 @@ import {
   standalone: true,
 })
 export class HtmlButtonDirective implements OnInit, OnChanges {
-  @Input() variant!:
-    | 'default'
+  @Input() variant:
     | 'primary'
     | 'secondary'
     | 'outline'
-    | 'destructive'
     | 'ghost'
-    | 'link';
-  @Input() color!: 'primary' | 'secondary' | 'help' | 'info' | 'success' | 'warning' | 'danger';
+    | 'link' = 'primary';
+  @Input() color!: 'info' | 'help' | 'success' | 'warning' | 'danger';
   @Input() size: 'sm' | 'md' | 'lg' | 'icon' = 'md';
   @Input() class: string = '';
 
@@ -45,45 +43,29 @@ export class HtmlButtonDirective implements OnInit, OnChanges {
 
   private getVariantClass(variant: string): string {
     switch (variant) {
-      case 'default':
-        return '';
       case 'primary':
-        return '';
+        return 'text-white bg-zinc-900 hover:bg-zinc-900/90 focus-visible:ring-zinc-400 shadow dark:text-zinc-900 dark:bg-neutral-50 dark:hover:bg-zinc-50/90';
       case 'secondary':
-        return '';
+        return 'text-zinc-900 bg-zinc-100 hover:bg-zinc-100/80 focus-visible:ring-zinc-400 shadow-sm dark:text-white dark:bg-zinc-800 dark:hover:bg-zinc-800/80';
       case 'outline':
-        return '';
-      case 'destructive':
-        return 'text-white bg-red-600 hover:bg-red-600/90 focus-visible:ring-zinc-400 shadow-sm';
+        return 'text-zinc-900 border border-zinc-200 bg-white hover:bg-zinc-100 focus-visible:ring-zinc-400 shadow-sm dark:text-neutral-50 dark:border-zinc-800 dark:bg-transparent dark:hover:bg-zinc-800';
       case 'ghost':
         return 'text-zinc-900 hover:bg-zinc-100 focus-visible:ring-zinc-400 dark:text-neutral-50 dark:hover:bg-zinc-800';
       case 'link':
         return 'text-zinc-900 underline-offset-4 hover:underline focus-visible:ring-zinc-400 dark:text-neutral-50';
       default:
-        return '';
+        return 'text-white shadow-sm bg-zinc-900 hover:bg-zinc-900/90 dark:text-zinc-900 dark:bg-neutral-50 dark:hover:bg-zinc-50/90';
     }
   }
 
   private getColorClass(color: string): string {
     
     switch (color) {
-      case 'primary':
-        return `${
-            this.variant === 'outline' ? 
-              'text-zinc-900 shadow-sm border border-zinc-200 bg-transparent hover:bg-zinc-100 dark:text-neutral-50 dark:border-zinc-800 dark:bg-transparent dark:hover:bg-zinc-800' : 
-              'text-white shadow-sm bg-zinc-900 hover:bg-zinc-900/90 dark:text-zinc-900 dark:bg-neutral-50 dark:hover:bg-zinc-50/90'
-        }`;
       case 'info':
         return `${
           this.variant === 'outline' ? 
             'text-blue-500 shadow-sm border border-blue-500 bg-transparent hover:bg-blue-500/5 dark:hover:bg-blue-500/10' : 
             'text-white shadow-sm bg-blue-500 hover:bg-blue-500/90'
-        }`;
-      case 'secondary':
-        return `${
-          this.variant === 'outline' ? 
-            'text-gray-800 shadow-sm border border-gray-800 bg-transparent hover:bg-gray-100 dark:text-gray-50 dark:border-zinc-800 dark:hover:bg-gray-800/10' : 
-            'text-zinc-900 bg-zinc-100 hover:bg-zinc-100/80 shadow-sm dark:text-white dark:bg-zinc-800 dark:hover:bg-zinc-800/80'
         }`;
       case 'help':
         return `${
