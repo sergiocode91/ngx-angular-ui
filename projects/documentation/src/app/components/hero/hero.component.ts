46cd7ffa0373fcb6fe64673838cfae9f53f7f3e6
sergiodev91 <sergiodev91@gmail.com>
2024-07-29 19:43:28 -0400

    ✨ [UPDATE] Add footer and badge version

--- projects/documentation/src/app/components/hero/hero.component.ts
+++ projects/documentation/src/app/components/hero/hero.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
-import { HtmlButtonDirective } from 'ngx-angular-ui';
+import { HtmlButtonDirective, HtmlBadgeDirective } from 'ngx-angular-ui';
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HtmlButtonDirective, LucideAngularModule, RouterModule],
+  imports: [
+    RouterModule,
+    LucideAngularModule,
+    HtmlButtonDirective,
+    HtmlBadgeDirective,
+  ],
   templateUrl: './hero.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
