b3ff119350a80302b117ea99ee4bb5c169abd51e
sergiodev91 <sergiodev91@gmail.com>
2024-08-15 15:01:07 -0400

    🔧 [UPDATE] Add sizes config

--- projects/ngx-angular-ui/src/lib/dialog/dialog-content.directive.ts
+++ projects/ngx-angular-ui/src/lib/dialog/dialog-content.directive.ts
@@ -13,6 +13,7 @@ import {
   standalone: true,
 })
 export class HtmlDialogContentDirective implements OnInit, OnChanges {
+  @Input() size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' = 'md';
   @Input() class: string = '';
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
@@ -25,9 +26,8 @@ export class HtmlDialogContentDirective implements OnInit, OnChanges {
   }
 
   private applyClasses() {
-    const baseClasses =
-      'grid w-full max-w-md shadow-md p-6 gap-5 rounded-lg border border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950';
-    const classes = `${baseClasses} ${this.class}`;
+    const sizeClass = this.getSizeClass(this.size);
+    const classes = `${sizeClass} ${this.class} grid max-w-md shadow-md p-6 gap-5 rounded-lg border border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950`;
 
     this.renderer.setAttribute(this.el.nativeElement, 'class', classes);
   }
@@ -35,5 +35,30 @@ export class HtmlDialogContentDirective implements OnInit, OnChanges {
   @HostListener('click', ['$event'])
   onClick(event: Event) {
     event.stopPropagation();
+  }
+
+  private getSizeClass(size: string): string {
+    switch (size) {
+      case 'xs':
+        return 'w-full max-w-xs';
+      case 'sm':
+        return 'w-full max-w-sm';
+      case 'md':
+        return 'w-full max-w-md';
+      case 'lg':
+        return 'w-full max-w-lg';
+      case 'xl':
+        return 'w-full max-w-xl';
+      case '2xl':
+        return 'w-full max-w-2xl';
+      case '3xl':
+        return 'w-full max-w-3xl';
+      case '4xl':
+        return 'w-full max-w-4xl';
+      case '5xl':
+        return 'w-full max-w-5xl';
+      default:
+        return 'w-full max-w-md';
+    }
   }
 }
